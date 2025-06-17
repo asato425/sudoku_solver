33b56b18f71618d83df0702e259570102d997c55
asato425 <suasa425@icloud.com>
2025-06-18 01:25:28 +0900

    .ideaフォルダに設定ファイルを追加し、テストケースを拡充

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -24,6 +24,123 @@ def test_solve_easy_board():
     print("Solved Board:")
     print_board(easy_board)
 
+def test_solve_almost_solved_board():
+    from sudoku.solver import solve
+    from sudoku.utils import print_board
+
+    almost_solved = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 0],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    ]
+
+    print("Original Board:")
+    print_board(almost_solved)
+
+    assert solve(almost_solved) is True
+
+    print("Solved Board:")
+    print_board(almost_solved)
+
+def test_solve_empty_board():
+    from sudoku.solver import solve
+    from sudoku.utils import print_board
+
+    empty_board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    print("Original Board:")
+    print_board(empty_board)
+
+    assert solve(empty_board) is True
+
+    print("Solved Board:")
+    print_board(empty_board)
+
+def test_solve_invalid_board():
+    from sudoku.solver import solve
+    from sudoku.utils import print_board
+
+    invalid_board = [
+        [5, 5, 0, 0, 7, 0, 0, 0, 0],  # Duplicate 5 in the row
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    print("Original Board:")
+    print_board(invalid_board)
+
+    assert solve(invalid_board) is False
+
+def test_solve_minimal_clues_board():
+    from sudoku.solver import solve
+    from sudoku.utils import print_board
+
+    minimal_clues = [
+        [0, 0, 0, 0, 0, 0, 0, 1, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    print("Original Board:")
+    print_board(minimal_clues)
+
+    assert solve(minimal_clues) is True
+
+    print("Solved Board:")
+    print_board(minimal_clues)
+
+def test_solve_one_missing_cell():
+    from sudoku.solver import solve
+    from sudoku.utils import print_board
+
+    one_missing = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 0]  # Only one cell missing
+    ]
+
+    print("Original Board:")
+    print_board(one_missing)
+
+    assert solve(one_missing) is True
+
+    print("Solved Board:")
+    print_board(one_missing)
+
 def test_solve_medium_board():
     from sudoku.solver import solve
     from sudoku.utils import print_board
